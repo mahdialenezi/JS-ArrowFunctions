564068d9f98b847c78cd3da77a1109c79747f97c
mahdialenezi <mahdialmanhal32@gmail.com>
2024-11-05 17:28:12 +0300

    done

---
+++ arrowFunctions.js
@@ -0,0 +1,25 @@
+// //Task 1
+
+
+// const helloArrows = () => {
+//     console.log("Hello, mahdi!");
+//   };
+  
+//   helloArrows();
+
+
+//Task 2
+
+// let sum = (a, b) => {
+//    return  a + b
+// };
+// console.log(sum(7,5));
+
+//Task 3
+
+
+const square = (a,b) => {
+    return a + b **2
+};
+console.log(square(7,5));
+
